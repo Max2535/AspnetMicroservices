d66df5abf572f228328eddd88ae4701909993f9a
suppchai <kalmro4@gmail.com>
2022-11-24 09:03:55 +0700

    end

--- src/Shopping.Aggregator/Shopping.Aggregator/Program.cs
+++ src/Shopping.Aggregator/Shopping.Aggregator/Program.cs
@@ -1,7 +1,11 @@
+using Common.Logging;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.OpenApi.Models;
+using Polly;
+using Polly.Extensions.Http;
+using Serilog;
 using Shopping.Aggregator.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,18 +13,28 @@ var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration; // allows both to access and to set up the config
 IWebHostEnvironment environment = builder.Environment;
 // Add services to the container.
+builder.Services.AddTransient<LoggingDelegatingHandler>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddHttpClient<ICatalogService, CatalogService>(c =>
-              c.BaseAddress = new Uri(configuration.GetValue<string>("ApiSettings:CatalogUrl")));
+                c.BaseAddress = new Uri(configuration.GetValue<string>("ApiSettings:CatalogUrl")))
+                .AddHttpMessageHandler<LoggingDelegatingHandler>()
+                .AddPolicyHandler(GetRetryPolicy())
+                .AddPolicyHandler(GetCircuitBreakerPolicy());
 
 builder.Services.AddHttpClient<IBasketService, BasketService>(c =>
-    c.BaseAddress = new Uri(configuration.GetValue<string>("ApiSettings:BasketUrl")));
+    c.BaseAddress = new Uri(configuration.GetValue<string>("ApiSettings:BasketUrl")))
+    .AddHttpMessageHandler<LoggingDelegatingHandler>()
+    .AddPolicyHandler(GetRetryPolicy())
+    .AddPolicyHandler(GetCircuitBreakerPolicy());
 
 builder.Services.AddHttpClient<IOrderService, OrderService>(c =>
-    c.BaseAddress = new Uri(configuration.GetValue<string>("ApiSettings:OrderingUrl")));
+    c.BaseAddress = new Uri(configuration.GetValue<string>("ApiSettings:OrderingUrl")))
+    .AddHttpMessageHandler<LoggingDelegatingHandler>()
+    .AddPolicyHandler(GetRetryPolicy())
+    .AddPolicyHandler(GetCircuitBreakerPolicy());
 
 builder.Services.AddControllers();
 
@@ -35,6 +49,35 @@ builder.Services.AddHealthChecks()
     .AddUrlGroup(new Uri($"{configuration.GetValue<string>("ApiSettings:BasketUrl")}/swagger/index.html"), "Basket.API", HealthStatus.Degraded)
     .AddUrlGroup(new Uri($"{configuration.GetValue<string>("ApiSettings:OrderingUrl")}/swagger/index.html"), "Ordering.API", HealthStatus.Degraded);
 
+IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+{
+    return HttpPolicyExtensions
+               .HandleTransientHttpError()
+               .CircuitBreakerAsync(
+                   handledEventsAllowedBeforeBreaking: 5,
+                   durationOfBreak: TimeSpan.FromSeconds(30)
+               );
+}
+
+IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+{
+    // In this case will wait for
+    //  2 ^ 1 = 2 seconds then
+    //  2 ^ 2 = 4 seconds then
+    //  2 ^ 3 = 8 seconds then
+    //  2 ^ 4 = 16 seconds then
+    //  2 ^ 5 = 32 seconds
+
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .WaitAndRetryAsync(
+            retryCount: 5,
+            sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+            onRetry: (exception, retryCount, context) =>
+            {
+                Log.Error($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
+            });
+}
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
